41da68ef6ddeb0c626d460f0f066de3c9bff7d15
Wojciech Kożuch <wkozuch@gmail.com>
2023-12-09 12:31:25 +0100

    Cleanup :)

--- AdventOfCode/Day9.cs
+++ AdventOfCode/Day9.cs
@@ -16,47 +16,12 @@ namespace AdventOfCode
 
     private static void Part1(IEnumerable<string> lines)
     {
-      var histories = lines.Select(l => l.Split(" ").Select(x => long.Parse(x)).ToArray()).ToList();
+      var histories = lines.Select(l => l.Split(" ").Select(long.Parse).ToArray()).ToList();
       long sum = 0;
       foreach (var h in histories)
       {
-        var deltas = new Dictionary<int, List<long>>();
-
         var d = h.ToList();
-        var n = new List<long>();
-        var level = 0;
-        while (d.Distinct().Count() != 1)
-        {
-          Console.WriteLine(level + ": " + string.Join(", ", d));
-          for (var i = 0; i < d.Count - 1; i++)
-          {
-            n.Add(d[i + 1] - d[i]);
-          }
-          //Console.WriteLine(level + 1 + ": " + string.Join(", ", n));
-          deltas.Add(level++, n);
-          d = n;
-          n = new List<long>();
-        }
-
-        var value = h.Last() + deltas.Sum(x => x.Value.Last());
-        Console.WriteLine("Sum: " + value + " = " + h.Last() + " + " + string.Join(" + ", deltas.Values.Select(x => x.Last())));
-        sum += value;
-      }
-
-      Console.WriteLine(sum); // 1882395907
-
-    }
-
-    private static void Part2(IEnumerable<string> lines)
-    {
-      var histories = lines.Select(l => l.Split(" ").Select(x => long.Parse(x)).ToArray()).ToList();
-      long sum = 0;
-      foreach (var h in histories)
-      {
-        var firstElements = new List<long>();
-
-        var d = h.ToList();
-        var value = h.First();
+        var value = d.Last();
         var level = 0;
         while (d.Distinct().Count() != 1)
         {
@@ -67,15 +32,42 @@ namespace AdventOfCode
             n.Add(d[i + 1] - d[i]);
           }
 
-          firstElements.Add(n.First());
+          value += n.Last();
+
           d = n;
         }
-        
-        for (var i = 0; i < firstElements.Count; i++)
+
+        sum += value;
+      }
+
+      Console.WriteLine(sum); // 1882395907
+
+    }
+
+    private static void Part2(IEnumerable<string> lines)
+    {
+      var histories = lines.Select(l => l.Split(" ").Select(long.Parse).ToArray()).ToList();
+      long sum = 0;
+      foreach (var h in histories)
+      {
+        var d = h.ToList();
+        var value = d.First();
+        var level = 0;
+        while (d.Distinct().Count() != 1)
         {
-          var sign = i % 2 == 0 ? -1 : 1;
-          value += sign* firstElements[i];
+          Console.WriteLine(level++ + ": " + string.Join(", ", d));
+          var n = new List<long>();
+          for (var i = 0; i < d.Count - 1; i++)
+          {
+            n.Add(d[i + 1] - d[i]);
+          }
+
+          var sign = level % 2 == 0 ? 1 : -1;
+          value += sign * n.First();
+
+          d = n;
         }
+
         sum += value;
       }
 
